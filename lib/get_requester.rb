5aba5dcb073d275d17f51e17a918a7653f91195c
github username <github email address>
2019-12-02 10:52:50 -0800

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,22 @@
-# Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+class GetRequester 
+    URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+    
+    def get_response_body
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+         response.body
+        
+    end 
+    def get_requester
+        # we use the JSON library to parse the API response into nicely formatted JSON
+           JSON.parse(self.get_response_body).collect do |program|
+            program["name"]  
+          end
+        end
+end 
+get_request = GetRequester.new
+puts get_request.get_requester
